49e326246430149965fc03e03a8cab19651e54ee
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-05-12 15:33:26 +0200

    test(parser): add tests for missing camera resolution
    Add tests to verify correct parser behavior when handling camera
    configuration files that are missing either the resolution width or
    height parameter.
    
    Also add the corresponding test configuration files.

--- tests/test.cpp
+++ tests/test.cpp
@@ -146,3 +146,21 @@ TEST_F(ParserConfigFileTest, TestParseIncompleteCameraConfig4) {
 
   EXPECT_THROW(parser.parseConfigFile(camera, sc, lc), Raytracer::ParseError);
 }
+
+TEST_F(ParserConfigFileTest, TestParseIncompleteCameraConfig5) {
+  _cfgFile = "tests/test_scenes/camera/missingResolutionWidth.cfg";
+  Raytracer::ParserConfigFile parser(_cfgFile, _plugins);
+  Raytracer::Camera camera;
+  Raytracer::ShapeComposite sc;
+  Raytracer::LightComposite lc;
+
+  EXPECT_EQ(camera.getWidth(), 0);
+
+  _cfgFile = "tests/test_scenes/camera/missingResolutionHeight.cfg";
+  Raytracer::ParserConfigFile parser2(_cfgFile, _plugins);
+  Raytracer::Camera camera2;
+  Raytracer::ShapeComposite sc2;
+  Raytracer::LightComposite lc2;
+
+  EXPECT_EQ(camera2.getHeight(), 0);
+}
